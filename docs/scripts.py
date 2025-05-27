1d091556490de75c99e153d3451bdc0bd9ef9c95
Antonis <antonisdevstuff@gmail.com>
2025-05-27 18:44:55 +0300

    add logo url to doc script

--- docs/scripts.py
+++ docs/scripts.py
@@ -1,7 +1,11 @@
 import subprocess
 
+logo_url = "https://github.com/ToniDevStuff/pyxora/blob/b1952e81ce8543ef66cf1fabd4ea7dc9ea93f3fe/pyxora/images/icon.png?raw=true"
+
 argv = [
     "pdoc", "--docformat=google",
+    "--logo",logo_url,
+    "--favicon",logo_url,
     "pyxora", "!pyxora.examples"
 ]
 
