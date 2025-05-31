60d0e9725e17c436450fe89729b9460202068cbb
Antonis <antonisdevstuff@gmail.com>
2025-05-31 23:26:54 +0300

    add docs README + logo

---
+++ docs/README.md
@@ -0,0 +1,28 @@
+# Pyxora Documentation
+
+## Getting Started
+
+Before you begin, make sure you have **pdoc** installed:
+
+```bash
+pip install pdoc
+```
+Navigate to the project directory:
+```bash
+cd path/to/pyxora
+```
+View documentation in the browser:
+
+```bash
+python -m docs
+```
+
+---
+
+Build static HTML documentation:
+
+```bash
+python -m docs --html
+```
+
+This will generate HTML files in the given directory, which you can host or share.
--- docs/scripts.py
+++ docs/scripts.py
@@ -1,21 +1,33 @@
 import subprocess
+import os
 
-logo_url = "https://github.com/ToniDevStuff/pyxora/blob/b1952e81ce8543ef66cf1fabd4ea7dc9ea93f3fe/pyxora/images/icon.png?raw=true"
-
-argv = [
+base_argv = [
     "pdoc", "--docformat=google",
-    "--logo",logo_url,
-    "--favicon",logo_url,
+    "--logo", "http://localhost:8000/logo.png",
+    "--favicon", "http://localhost:8000/icon.png",
     "pyxora", "!pyxora.examples"
 ]
 
 def run():
-    subprocess.run(argv)
+    # create a http server to host the images
+    server = subprocess.Popen(
+        ["python", "-m", "http.server","8000","--directory","docs/data"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+    try:
+        subprocess.run(base_argv)
+    finally:
+        server.terminate()
 
 def build():
-    import os
-    output = os.path.abspath(input("output-path: ")+"/build")
-    argv.append("-o")
-    argv.append(output)
+    import shutil
+    base_argv[3] ="/data/logo.png" # logo
+    base_argv[5] ="/data/icon.png" # favicon
+    output = os.path.abspath(input("Output path (folder): ") + "/build")
+    argv = base_argv + ["-o", output]
     subprocess.run(argv)
+    shutil.copytree("docs/data", os.path.abspath(os.path.join(output, "data")), dirs_exist_ok=True)
+
     print("Done")
