081d76dde023fb646a6a06d1a3f594f709e23ed2
Antonis <antonisdevstuff@gmail.com>
2025-05-31 23:36:20 +0300

    fix docs

--- docs/scripts.py
+++ docs/scripts.py
@@ -1,33 +1,22 @@
 import subprocess
-import os
 
-base_argv = [
+logo_url = "https://github.com/ToniDevStuff/pyxora/blob/main/pyxora/data/images/logo.png?raw=true"
+icon_url = "https://github.com/ToniDevStuff/pyxora/blob/main/pyxora/data/images/icon.png?raw=true"
+
+argv = [
     "pdoc", "--docformat=google",
-    "--logo", "http://localhost:8000/logo.png",
-    "--favicon", "http://localhost:8000/icon.png",
+    "--logo",logo_url,
+    "--favicon",icon_url,
     "pyxora", "!pyxora.examples"
 ]
 
 def run():
-    # create a http server to host the images
-    server = subprocess.Popen(
-        ["python", "-m", "http.server","8000","--directory","docs/data"],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL
-    )
-
-    try:
-        subprocess.run(base_argv)
-    finally:
-        server.terminate()
+    subprocess.run(argv)
 
 def build():
-    import shutil
-    base_argv[3] ="/data/logo.png" # logo
-    base_argv[5] ="/data/icon.png" # favicon
-    output = os.path.abspath(input("Output path (folder): ") + "/build")
-    argv = base_argv + ["-o", output]
+    import os
+    output = os.path.abspath(input("output-path: ")+"/build")
+    argv.append("-o")
+    argv.append(output)
     subprocess.run(argv)
-    shutil.copytree("docs/data", os.path.abspath(os.path.join(output, "data")), dirs_exist_ok=True)
-
     print("Done")
